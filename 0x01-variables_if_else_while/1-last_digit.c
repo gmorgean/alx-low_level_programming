debca1f77a7e831b4be75ebb37b0545e7e43fdf4
gmorgean <gmorgean@gmail.com>
2023-03-31 10:10:54 -0700

    last mod

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,4 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ *
+ * Return: always return 0
+ *
+ */
+int main(void)
+{
 	int n;
+	int lastn;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
